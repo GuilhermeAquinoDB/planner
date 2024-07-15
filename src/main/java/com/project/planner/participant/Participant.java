2f71da3a2f5cea84d75a911c8dd86eebff930046
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2024-07-14 21:22:43 -0300

    :sparkles: feat: table, entity and repository Participant

---
+++ src/main/java/com/project/planner/participant/Participant.java
@@ -0,0 +1,37 @@
+package com.project.planner.participant;
+
+import com.project.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "participants")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column( nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
